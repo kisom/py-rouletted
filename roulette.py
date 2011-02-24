0bbffbd5de2fd7f053ac38e920954c68bd500646
Kyle Isom <coder@kyleisom.net>
2011-02-24 13:45:43 -0700

    add get_user_process_list

--- roulette.py
+++ roulette.py
@@ -3,11 +3,16 @@ import subprocess
 import sys
 
 
+def die(errstr):
+    if errstr:
+        sys.stderr.write("%s\n" % errstr)
+    sys.exit(1)
+
 # uses who and uniq to grab a unique user list. optionally, pass in a list of sysadmins
 # to not kill. note this only kills users logged in interactively, which is the point
 # of this little game.
 def get_random_user(exclude_list = None):
-    command = "who | awk '{ print $1 }' | uniq" 
+    command = "who | awk '{ print $1 }' | uniq | xargs" 
 
     p = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE)
 
@@ -19,13 +24,29 @@ def get_random_user(exclude_list = None):
         userlist = [ user for user in userlist if user ] # remove blank entries
 
     if not userlist:
-        sys.stderr.write('empty user list...')        
+        err = 'empty user list...'        
         if exclude_list:
-            sys.stderr.write(' maybe try making the safe list a little less... safe?')
-        sys.stderr.write('\n');
-        sys.exit(1)
+            err += ' maybe try making the safe list a little less... safe?'
+
+        die(err)
 
     return random.choice(userlist)
+
+# if you don't pass in a user as a string, you get whatever is coming to you
+def get_user_process_list(user = None):
+    if not user:
+        die('try passing a user?')
+
+    cmd = 'pgrep -U %s | xargs' % user
+
+    p = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE)
+    proclist = p.communicate()[0].strip()
+
+    return proclist
+
+def kill_users_procs(user = None):
+    if not user:
+        die('you probably shouldn\'t be running this...')
 
 print get_random_user(['kisom'])
 
